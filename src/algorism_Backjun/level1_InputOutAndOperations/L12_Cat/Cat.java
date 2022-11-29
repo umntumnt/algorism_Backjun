7158fa9ae6899c2398b4c3d1464ba0b5d634c421
KimMinSub <umnt6626@gmail.com>
2022-11-29 21:53:57 +0900

    2022.11.29 commit

---
+++ src/algorism_Backjun/level1_InputOutAndOperations/L12_Cat/Cat.java
@@ -0,0 +1,18 @@
+package algorism_Backjun.level1_InputOutAndOperations.L12_Cat;
+
+import java.util.*;
+public class Cat {
+
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		cat.solution();
+	}
+	
+	private void solution() {
+		System.out.println("\\    /\\"); // [\->\\]
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
---
+++ src/algorism_Backjun/level1_InputOutAndOperations/L13_dog/Dog.java
@@ -0,0 +1,19 @@
+package algorism_Backjun.level1_InputOutAndOperations.L13_dog;
+
+import java.util.*;
+public class Dog {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.solution();
+	}
+	
+	private void solution() {
+		System.out.println("|\\_/|"); // [\->\\]
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\"); // [" -> "\]
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
